e1c333bd550a821dfbac66bd1dffd5a42aebcf5f
jinseon <bongjs44@github.com>
2019-04-15 15:26:20 +0900

    add src

--- src/main/java/org/jinseon/examples/HelloServlet.java
+++ src/main/java/org/jinseon/examples/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// 데이터를 만든다.
-		String name = "김솔이";
+		String name = "봉진선";
 
 		// 데이터를 request에 태운다.
 		request.setAttribute("name", name);
